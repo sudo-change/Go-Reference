e234324c059b59fb95fd0e9ee9ceaba76d2ece56
sudo change <dragoondranzerkai@gmail.com>
2022-03-04 21:22:29 +0530

    rand

---
+++ other/random.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	rand.Seed(time.Now().UnixNano())
+	// rand.Seed(86)
+	// seeding with the same value returns same random sequence each run
+	// seed with different value like time.Now().UnixNano()
+	fmt.Println("my favourite number is:", rand.Intn(10))
+
+}
